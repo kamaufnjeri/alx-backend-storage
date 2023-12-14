9764453cb89458be3ab6aa5791a977c76cf40069
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-12-14 18:10:57 +0300

    list bands of Lam rock and ranked by ;ongetivity

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,13 +1,12 @@
--- Create a temporary table to store the aggregated fan counts per country
+-- Write a SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
 CREATE TEMPORARY TABLE IF NOT EXISTS temporary_origin_fans AS
     SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin;
 
--- Rank the countries based on the total number of fans in descending order
 SELECT origin, nb_fans
 FROM temporary_origin_fans
 ORDER BY nb_fans DESC;
 
--- Drop the temporary table
 DROP TEMPORARY TABLE IF EXISTS temporary_origin_fans;
