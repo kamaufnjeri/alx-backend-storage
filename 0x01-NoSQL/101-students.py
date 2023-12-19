f8b1766c4318a87ee06a32d72de1f16095cfdbda
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-12-19 20:38:22 +0300

    Get all students by marks

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+"""top students"""
+
+
+def top_students(mongo_collection):
+    """top students"""
+    students = mongo_collection.find()
+
+    for student in students:
+        total_score = 0
+        num_topics = 0
+
+        for topic in student.get('topics', []):
+            total_score += topic.get('score', 0)
+            num_topics += 1
+
+        if num_topics > 0:
+            average_score = total_score / num_topics
+            student['averageScore'] = round(average_score, 2)
+        else:
+            student['averageScore'] = 0
+
+    # Sorting students by averageScore in descending order
+    sorted_students = sorted(students, key=lambda x: x['averageScore'], reverse=True)
+
+    return sorted_students
+
+# Rest of the code remains unchanged
+
