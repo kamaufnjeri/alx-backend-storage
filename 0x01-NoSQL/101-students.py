f33b6329d8d0adac00a466c4c718c4ddb9e61dd5
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-12-19 20:53:17 +0300

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,8 +3,11 @@
 
 
 def top_students(collection):
-   """Use aggregation to calculate the average score for each student and sort them in descending order.
-    # The result includes only the 'name' and 'averageScore' fields."""
+    """
+    Use aggregation to calculate the average score for
+    each student and sort them in descending order.
+    # The result includes only the 'name' and 'averageScore' fields.
+    """
     aggregation_pipeline = [
         {
             "$project": {
@@ -19,5 +22,3 @@ def top_students(collection):
     sorted_students_cursor = collection.aggregate(aggregation_pipeline)
 
     return sorted_students_cursor
-
-
