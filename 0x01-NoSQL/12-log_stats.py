eab8b572b3d6b79bfcac69734d95a38e74599f6b
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-12-19 21:16:07 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
-'''Task 12's module.
-'''
+"""
+nginx_stats module
+"""
+
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
-    '''Prints stats about Nginx request logs.
-    '''
-    print('{} logs'.format(nginx_collection.count_documents({})))
-    print('Methods:')
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
-    status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
-    ))
-    print('{} status check'.format(status_checks_count))
-
-
-def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
-    '''
+def nginx_stats():
+    """
+    Provides some stats about Nginx logs stored in MongoDB.
+    """
+    # Connect to MongoDB
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+
+    # Access the collection
+    nginx_collection = client.logs.nginx
+
+    # Get the total number of logs
+    total_logs = nginx_collection.count_documents({})
+
+    # Display total logs
+    print(f"{total_logs} logs where {total_logs} is the number of documents in this collection")
+
+    # Display methods
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod={method}: {count}")
+
+    # Display specific log count
+    status_count = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"\tmethod=GET path=/status: {status_count}")
 
 
-if __name__ == '__main__':
-    run()
+if __name__ == "__main__":
+    nginx_stats()
