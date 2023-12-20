f29e1b40fa7d9f02823c2cfb8ca22d672c66ead7
Florence Njeri Kamau <115622982+kamaufnjeri@users.noreply.github.com>
2023-12-21 02:35:11 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python3
-'''Module with tools for request caching and tracking.
-'''
+""" Redis Module """
 
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-redis_instance = redis.Redis()
-'''The module-level Redis instance.
-'''
+redis_ = redis.Redis()
 
 
-def cache_data(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
     @wraps(method)
-    def invoke_and_cache(url) -> str:
-        '''Wrapper function for caching the output.
-        '''
-        redis_instance.incr(f'request_count:{url}')
-        cached_result = redis_instance.get(f'cached_result:{url}')
-        if cached_result:
-            return cached_result.decode('utf-8')
-        result = method(url)
-        redis_instance.set(f'request_count:{url}', 0)
-        redis_instance.setex(f'cached_result:{url}', 10, result)
-        return result
-    return invoke_and_cache
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
-@cache_data
-def fetch_page_content(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
