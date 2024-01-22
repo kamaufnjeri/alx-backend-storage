90add7ca4f418c4b1dcef03f6f93d6d744ae95d0
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-01-22 14:30:25 +0300

    mongodb alx project tasks

--- 0x01-NoSQL/8-all.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" list all documents with python"""
-
-
-def list_all(mongo_collection):
-    """ list all documents"""
-    documents = mongo_collection.find()
-
-    return documents
--- 0x01-NoSQL/README.md
+++
@@ -1,1 +0,0 @@
-Tasks for the project NoSQL with mongodb
